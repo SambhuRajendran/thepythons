3d9a510592d61d1536b73f6a82d8a941c8cf0c8d
sambhu619 <rsambhu619@gmail.com>
2019-08-18 18:15:15 +0530

    first commit

---
+++ examples_/array.py
@@ -0,0 +1,24 @@
+
+#listof get input from the user
+listof = []
+#n is the number of values  that user wants to add to array
+n= int(input("Enter the length of your list: "))
+
+for i in range(0,n):
+    x = int(input('Enter the numbers? '))
+    #Appends the value entered by user to the 'listof' variable
+    listof.append(x)
+    print(x)
+
+size = len(listof)
+print('Original List: ', listof)
+#storing the value of 0th position to a temp variable
+temp = listof[0]
+#Then replacing the first value with the (n-1)th value of the size
+listof[0]=listof[size - 1]
+#changing the (n-1)th value to the temp variable
+listof[size - 1]= temp
+#saving the value of temp to 0th value of the list
+temp = listof[0]
+print('New swapped  list: ', listof)
+
