c9491e4cd6a88a478f402685bb50e18a88e3bbcd
sambhu619 <rsambhu619@gmail.com>
2021-03-06 16:48:44 -0500

    added Comments

--- Basics/set.py
+++ Basics/set.py
@@ -3,6 +3,6 @@ room2 = {'jibu', 'shab'}
 room_friends.add('jibu')
 
 print(room_friends)
-
+#Sets are case sensitive, here the name Jibu is added because letter 'J' is capitalized. 
 room2.add('Jibu')
 print(room2)
