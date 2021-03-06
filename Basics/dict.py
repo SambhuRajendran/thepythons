9f924c16ab6670324687f36257e2c749f7dcb601
sambhu619 <rsambhu619@gmail.com>
2021-03-06 17:58:48 -0500

    collectionof dict

--- Basics/dict.py
+++ Basics/dict.py
@@ -1,4 +1,12 @@
+
 friend_ages = {'Rekhin': 35, 'Jephin': 14, }
 friend_ages["Rekhin"] = 10
-
 print(friend_ages)
+
+friends = (
+    {"name": "Sambhu", "age": 28},
+    {"name": "KP", "age": 29},
+    {"name": "Jephin", "age": 22}
+)
+#the following print statement will return the value of the first key (name) i.e Sambhu
+print(friends[0]["name"])
