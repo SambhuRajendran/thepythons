ad063287871c2547a06d7453015230649197f5c1
sambhu619 <rsambhu619@gmail.com>
2021-03-06 17:13:44 -0500

    Advanced Set

--- Basics/set.py
+++ Basics/set.py
@@ -3,6 +3,16 @@ room2 = {'jibu', 'shab'}
 room_friends.add('jibu')
 
 print(room_friends)
-#Sets are case sensitive, here the name Jibu is added because letter 'J' is capitalized. 
+#Sets are case sensitive, here the name Jibu is added because its case sensitive
 room2.add('Jibu')
 print(room2)
+
+art_friends = {'Jibu', 'Rekhin', 'sambhu'}
+science = {'Rekhin', 'Jephin'}
+
+art_friends_not_science = art_friends.difference(science)
+science_not_art = science.difference(art_friends)
+
+print(art_friends_not_science)
+print(science_not_art)
+
