4f3c72992142c9a5acac41a29f9fa7b637ad9943
sambhu619 <rsambhu619@gmail.com>
2021-03-07 16:33:48 -0500

    Updated Comment

--- Basics/if_else.py
+++ Basics/if_else.py
@@ -4,6 +4,7 @@ friends2 = ['KP', 'Jephin']
 
 user_input = input('Enter a username: ')
 
+#The follwing condition checks in if and elif will look for the values in variables friends and friends2
 if user_input in friends:
     print(f'Hello {user_input}!')
 elif user_input in friends2:
