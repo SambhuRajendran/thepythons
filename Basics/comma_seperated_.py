f77aa66602315a7f1ae4b76673866deddee7afdf
sambhu619 <rsambhu619@gmail.com>
2021-03-07 16:06:42 -0500

    added new comment

--- Basics/comma_seperated_.py
+++ Basics/comma_seperated_.py
@@ -1,3 +1,5 @@
+#how to join a list in a user-friendly format
+
 friends = ["KP", "Rekhin", "Jibu", "Jephin"]
 #creating a variable named comma seperated to print the values in a user-friendly format
 comma_seperated = ", ".join(friends)
