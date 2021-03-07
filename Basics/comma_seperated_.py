affd2bc1466cd50658f9f4e2f2a190c08a3292ff
sambhu619 <rsambhu619@gmail.com>
2021-03-07 16:04:50 -0500

    comma seperated program

---
+++ Basics/comma_seperated_.py
@@ -0,0 +1,4 @@
+friends = ["KP", "Rekhin", "Jibu", "Jephin"]
+#creating a variable named comma seperated to print the values in a user-friendly format
+comma_seperated = ", ".join(friends)
+print(f'My friends are {comma_seperated}.')
