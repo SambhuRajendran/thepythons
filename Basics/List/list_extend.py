e08da7deb39ab219bc14049ebc9f55d73d4ab931
sambhu619 <rsambhu619@gmail.com>
2021-03-10 16:41:36 -0500

    extend

---
+++ Basics/List/list_extend.py
@@ -0,0 +1,6 @@
+#This program shows how to append values of list from one to another
+mufc = ["De Gea", "Rashford", "Cavani", "Bruno"]
+madrid = ["Casmeiro", "Kroos", "Hazard"]
+mufc.extend(madrid)
+
+print(mufc)
