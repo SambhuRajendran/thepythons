8d9dde1716fe7a165b6b7e283b4d944e592c2b8e
sambhu619 <rsambhu619@gmail.com>
2021-03-10 17:27:16 -0500

    Notes_updated

--- Basics/List/list_extend.py
+++ Basics/List/list_extend.py
@@ -4,7 +4,6 @@ madrid = ["Casmeiro", "Kroos", "Hazard"]
 mufc.extend(madrid)
 #here the values from madrid list will be added to mufc
 print(mufc)
-
 #extend can also add elements from Sets, dictionary, tuple etc
 #Let's create a new tuple
 barca = ('Messi', 'Busquets', 'Dembele')
