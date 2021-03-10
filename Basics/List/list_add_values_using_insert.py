1b697ed69a9802959f2a8a8e8a8074ca88d32b52
sambhu619 <rsambhu619@gmail.com>
2021-03-10 16:38:03 -0500

    insert

---
+++ Basics/List/list_add_values_using_insert.py
@@ -0,0 +1,12 @@
+#here we are going to replace and  add values to a list
+
+fifa = ["Ronaldo", "Messi", "Rooney", "Mane", "De Gea"]
+#in the below line the value Rashford will replace at postion 1
+# the postion [2] actually means [1] so player Pogba will be added
+fifa[1:2] = ["Rashford", "Pogba"]
+print(fifa)
+
+#Here you can insert a value without replacing the current ones
+fifa.insert(3, "Tevez")
+#Value Tevez will be inserted after Rooney
+print(fifa)
