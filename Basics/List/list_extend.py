42296ea006e5609e37dfb21d9bc113489f3fdb68
sambhu619 <rsambhu619@gmail.com>
2021-03-10 17:01:09 -0500

    combining list with a tuple

--- Basics/List/list_extend.py
+++ Basics/List/list_extend.py
@@ -4,3 +4,10 @@ madrid = ["Casmeiro", "Kroos", "Hazard"]
 mufc.extend(madrid)
 #here the values from madrid list will be added to mufc
 print(mufc)
+
+#extend can also add elements from Sets, dictionary, tuple etc
+#Let's create a new tuple
+barca = ('Messi', 'Busquets', 'Dembele')
+#This will add tuple barca to previously extended list
+mufc.extend(barca)
+print(mufc)
