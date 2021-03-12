edb6697ddb455c4e3db9305d6aaecce0965c0b78
sambhu619 <rsambhu619@gmail.com>
2021-03-11 23:33:20 -0500

    loop_lists

---
+++ Basics/List/for_loop_list.py
@@ -0,0 +1,8 @@
+thislist = ["Cat", "Dog", "Goose"]
+for animals in thislist:
+    print(animals)
+
+#A list can be also printed by refering their index number
+
+for animal in range(len(thislist)):
+    print(thislist[animal])
---
+++ Basics/List/list_loop_while.py
@@ -0,0 +1,9 @@
+#here we are using len() to determine the length of the list
+#Then we will start the list at index 0
+
+thelist = ["Item1", "item2", "item3", "item4"]
+i = 0
+print(len(thelist))
+while i < len(thelist):
+    print(thelist[i])
+    i = i+1
